3d0c30687e7234dc5de50e1044b7c5530efc0c8f
Patrick Sime <psime86@gmail.com>
2020-01-27 20:49:15 -0600

    YouTube API push

--- assets/javascript/api-youtube.js
+++ assets/javascript/api-youtube.js
@@ -5,11 +5,11 @@ $("#search-btn").on("click", function(event){
 
 event.preventDefault();
 // Var for search bar input
-var video = $("#first_name2").val().trim();
-console.log(video);
+var search = $("#search-field").val().trim();
+console.log(search);
 
 // YoutTube API and AJAX call
-    var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + video +
+    var queryURL = "https://www.googleapis.com/youtube/v3/search?part=snippet&q=" + search +
          "&key=AIzaSyB21l2vM6B11Bltdq3Yj_YI7jetMu9ZjLo&limit=5";
 
          $.ajax({
@@ -26,7 +26,20 @@ console.log(video);
             // For loop to run through response
             for (var i = 0; i < response.items.length; i++){
                 var videoResult = response.items[i].id.videoId;
+                var videoTitle = response.items[i].snippet.title;
+                var thumbnail = response.items[i].snippet.thumbnails.default.url;
+                var video = $("<img class= 'newVideo'>");
+                var newVidDiv = $("<div class ='newVidDiv'>");
+                var titleDiv = $("<p>").html("Title: " + videoTitle);
                 console.log(videoResult);
+                console.log(videoTitle);
+                console.log(thumbnail);
+                
+                video.attr("src", "https://www.youtube.com/watch?v=" + videoResult);
+                console.log(video);
+                newVidDiv.append(titleDiv);
+                newVidDiv.prepend(video);
+                $("#video-div").prepend(newVidDiv);
             }
 
 
