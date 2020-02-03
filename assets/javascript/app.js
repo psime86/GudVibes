ba6df759ce6b18ca6eede0966a52b9921a6f6600
David Koeller <david.koeller@gmail.com>
2020-02-02 19:58:24 -0600

    deleted obsolete code, changed title

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,20 +1,9 @@
+//Materialize Javascript
 $(document).ready(function(){
     $('.sidenav').sidenav({
       edge: "right"
     });
   });
-
-//shuffle button click event
-$("#shuffle-btn").on("click", function() {
-  console.log("shuffle");
-
-});
-
-//play button click event
-// $("#play-btn").on("click", function() {
-//   console.log("play");
-
-// });
 
 //delete button click event
 $("#delete-btn").on("click", function() {
